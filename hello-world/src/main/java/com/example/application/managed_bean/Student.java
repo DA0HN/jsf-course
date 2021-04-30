cfd28d1d5e76997b1ea258babc8952467ea04410
Gabriel Honda <gabrielhondacba@gmail.com>
2021-04-29 23:38:04 -0400

    fix: adiciona as anotações necessárias para gerenciamento do bean Student

--- hello-world/src/main/java/com/example/application/managed_bean/Student.java
+++ hello-world/src/main/java/com/example/application/managed_bean/Student.java
@@ -1,7 +1,10 @@
 package com.example.application.managed_bean;
 
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import java.io.Serializable;
 
 /**
  * Created by daohn on 29/04/2021
@@ -9,7 +12,8 @@ import javax.inject.Named;
  * @since 29/04/2021
  */
 @Named
-public class Student {
+@SessionScoped
+public class Student implements Serializable {
 
   private String firstName;
   private String lastName;
@@ -27,13 +31,21 @@ public class Student {
   public String getFirstName() {
     return firstName;
   }
+
   public void setFirstName(String firstName) {
     this.firstName = firstName;
   }
+
   public String getLastName() {
     return lastName;
   }
+
   public void setLastName(String lastName) {
     this.lastName = lastName;
+  }
+
+  @PostConstruct
+  public void init() {
+    System.out.println("Student Bean has been created!");
   }
 }
