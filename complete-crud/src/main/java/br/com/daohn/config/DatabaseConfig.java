d1cbc34d2719216b2af35683df3e67edb59ff739
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-02 15:25:07 -0400

    fix: corrige criação do produtor de EntityManager

--- complete-crud/src/main/java/br/com/daohn/config/DatabaseConfig.java
+++ complete-crud/src/main/java/br/com/daohn/config/DatabaseConfig.java
@@ -1,20 +1,20 @@
 package br.com.daohn.config;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 
 /**
  * Created by daohn on 02/05/2021
  * @author daohn
  * @since 02/05/2021
  */
-
+@ApplicationScoped
 public class DatabaseConfig {
-
   @Produces
-  public EntityManager getEntityManager() {
-    return Persistence.createEntityManagerFactory("student").createEntityManager();
-  }
+  @PersistenceContext(unitName = "student")
+  private EntityManager entityManager;
 
 }
