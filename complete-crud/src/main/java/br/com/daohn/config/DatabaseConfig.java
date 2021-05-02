6a3eb95e83b5f1d0441f51df2f4af5fe740b2f29
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-02 15:12:43 -0400

    wip: adiciona criação de instância do EntityManager

--- complete-crud/src/main/java/br/com/daohn/config/DatabaseConfig.java
+++ complete-crud/src/main/java/br/com/daohn/config/DatabaseConfig.java
@@ -1,9 +1,20 @@
 package br.com.daohn.config;
 
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
 /**
  * Created by daohn on 02/05/2021
  * @author daohn
  * @since 02/05/2021
  */
+
 public class DatabaseConfig {
+
+  @Produces
+  public EntityManager getEntityManager() {
+    return Persistence.createEntityManagerFactory("student").createEntityManager();
+  }
+
 }
