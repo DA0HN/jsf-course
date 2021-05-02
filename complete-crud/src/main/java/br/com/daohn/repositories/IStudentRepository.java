a154735ab337cf916a680ace03ef2a8a09d17884
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-02 15:13:09 -0400

    wip: adiciona implementação da interface IStudentRepository

--- complete-crud/src/main/java/br/com/daohn/repositories/IStudentRepository.java
+++ complete-crud/src/main/java/br/com/daohn/repositories/IStudentRepository.java
@@ -1,9 +1,18 @@
 package br.com.daohn.repositories;
 
+import br.com.daohn.model.Student;
+
+import java.util.List;
+
 /**
  * Created by daohn on 02/05/2021
  * @author daohn
  * @since 02/05/2021
  */
-public class IStudentRepository {
+public interface IStudentRepository {
+  void save(Student student);
+  void update(Student student);
+  void delete(Student student);
+  Student findById(Long id);
+  List<Student> findAll();
 }
--- complete-crud/src/main/java/br/com/daohn/repositories/StudentRepository.java
+++ complete-crud/src/main/java/br/com/daohn/repositories/StudentRepository.java
@@ -1,9 +1,45 @@
 package br.com.daohn.repositories;
 
+import br.com.daohn.model.Student;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import java.util.List;
+
 /**
  * Created by daohn on 02/05/2021
  * @author daohn
  * @since 02/05/2021
  */
-public class StudentRepository {
+@Named("repository")
+@ApplicationScoped
+public class StudentRepository implements IStudentRepository {
+
+  private EntityManager manager;
+
+  @Inject
+  public StudentRepository(EntityManager manager) {
+    this.manager = manager;
+  }
+
+  @Override public void save(Student student) {
+  }
+
+  @Override public void update(Student student) {
+
+  }
+
+  @Override public void delete(Student student) {
+
+  }
+
+  @Override public Student findById(Long id) {
+    return null;
+  }
+
+  @Override public List<Student> findAll() {
+    return null;
+  }
 }
