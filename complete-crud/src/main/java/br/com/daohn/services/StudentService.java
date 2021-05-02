f17866f452472272a30b462253c3831fcd8903d1
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-02 15:16:17 -0400

    wip: adiciona implementação da interface IStudentService

--- complete-crud/src/main/java/br/com/daohn/services/StudentService.java
+++ complete-crud/src/main/java/br/com/daohn/services/StudentService.java
@@ -1,9 +1,26 @@
 package br.com.daohn.services;
 
+import br.com.daohn.repositories.IStudentRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
 /**
  * Created by daohn on 02/05/2021
  * @author daohn
  * @since 02/05/2021
  */
-public class StudentService {
+@Named("service")
+@ApplicationScoped
+public class StudentService implements IStudentService {
+
+  private IStudentRepository repository;
+
+  @Inject
+  public StudentService(@Named("repository") IStudentRepository repository) {
+    this.repository = repository;
+  }
+
+
 }
