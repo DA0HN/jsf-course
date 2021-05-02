326606c85159e5de635e679ab99d192f8e02de21
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-01 20:31:47 -0400

    feat: adiciona formulário utilizando dropdown

---
+++ hello-world/src/main/java/com/example/application/managed_bean/StudentDropDown.java
@@ -0,0 +1,53 @@
+package com.example.application.managed_bean;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+/**
+ * Created by daohn on 01/05/2021
+ * @author daohn
+ * @since 01/05/2021
+ */
+@Named
+@SessionScoped
+public class StudentDropDown implements Serializable {
+
+  private String firstName;
+  private String lastName;
+
+  private String country;
+
+  public StudentDropDown() {
+  }
+
+  public StudentDropDown(String firstName, String lastName, String country) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.country = country;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+}
