be577bdcbdaba22ac5c004049a81c3e263ebd045
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-06 10:12:49 -0400

    feat: adiciona anotações JPA para a entidade Student

--- complete-crud/src/main/java/br/com/daohn/model/Student.java
+++ complete-crud/src/main/java/br/com/daohn/model/Student.java
@@ -1,11 +1,21 @@
 package br.com.daohn.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by daohn on 02/05/2021
  * @author daohn
  * @since 02/05/2021
  */
+@Entity
 public class Student {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
   private String firstName;
   private String lastName;
@@ -36,5 +46,13 @@ public class Student {
 
   public void setEmail(String email) {
     this.email = email;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
   }
 }
