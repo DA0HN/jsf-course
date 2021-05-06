7db68540124f4f11fda92feebaf6ec847d955aa5
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-06 16:40:55 -0400

    wip: adiciona carregamento inicial de estudantes

--- complete-crud/src/main/java/br/com/daohn/bean/StudentController.java
+++ complete-crud/src/main/java/br/com/daohn/bean/StudentController.java
@@ -1,11 +1,13 @@
 package br.com.daohn.bean;
 
+import br.com.daohn.model.Student;
 import br.com.daohn.services.IStudentService;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by daohn on 02/05/2021
@@ -18,8 +20,18 @@ public class StudentController implements Serializable {
 
   private final IStudentService service;
 
+  private List<Student> students;
+
   @Inject
   public StudentController(@Named("service") IStudentService service) {
     this.service = service;
+  }
+
+  public List<Student> getStudents() {
+    return students;
+  }
+
+  public void loadStudent() {
+    this.students = this.service.findAll();
   }
 }
