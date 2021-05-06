96787bc68c54114f95f6cb1c5477aba8cec5d252
Gabriel Honda <gabrielhondacba@gmail.com>
2021-05-06 16:45:38 -0400

    feat: adiciona Qualifier para injeção de dependência da SessionFactory

---
+++ complete-crud/src/main/java/br/com/daohn/repositories/DatabaseQualifier.java
@@ -0,0 +1,19 @@
+package br.com.daohn.repositories;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by daohn on 06/05/2021
+ * @author daohn
+ * @since 06/05/2021
+ */
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD,
+          ElementType.TYPE, ElementType.PARAMETER})
+public @interface DatabaseQualifier {
+}
