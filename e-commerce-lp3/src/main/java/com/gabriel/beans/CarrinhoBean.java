94ab4ce14a30a77302f6f6fb189f548e642820e4
Gabriel Honda <gabrielhondacba@gmail.com>
2021-06-26 12:00:46 -0400

    refactor: ajusta lógica para adição de produto no carrinho

--- e-commerce-lp3/src/main/java/com/gabriel/beans/CarrinhoBean.java
+++ e-commerce-lp3/src/main/java/com/gabriel/beans/CarrinhoBean.java
@@ -3,54 +3,44 @@ package com.gabriel.beans;
 import com.gabriel.entities.Item;
 import com.gabriel.entities.Produto;
 
-import javax.faces.view.ViewScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author daohn
  * @since 21/06/2021
  */
 @Named
-@ViewScoped
+@SessionScoped
 public class CarrinhoBean implements Serializable {
-  private List<Item> itens;
+  private Map<Produto, Item> itens;
 
   public CarrinhoBean() {
-    itens = new ArrayList<>();
+    itens = new HashMap<>();
   }
 
   public List<Item> getItens() {
-    return itens;
+    return new ArrayList<>(itens.values());
   }
 
-  public void setItens(List<Item> itens) {
+  public void setItens(Map<Produto, Item> itens) {
     this.itens = itens;
   }
 
-  public String adicionarProdutoNoCarrinho(Produto produto) {
-    int indice = existeItem(produto);
-    if(indice == -1) {
-      itens.add(new Item(produto, 1));
+  public String addProdutoInCart(Produto produto) {
+
+    if(itens.containsKey(produto)) {
+      var item = itens.get(produto);
+      item.incrementQuantidade();
     }
     else {
-      int qtde = this.itens.get(indice).getQuantidade() + 1;
-      this.itens.get(indice).setQuantidade(qtde);
+      itens.put(produto,Item.createItemWithZeroQuantidade(produto));
     }
-
     return "cart?faces-redirect=true";
   }
-
-  private int existeItem(Produto produto) {
-    for(int i = 0; i < itens.size(); i++) {
-      if(itens.get(i).getProduto().getId().equals(produto.getId())) {
-        return i;
-      }
-    }
-    return -1;
-
-  }
-
 }
--- e-commerce-lp3/src/main/java/com/gabriel/entities/Item.java
+++ e-commerce-lp3/src/main/java/com/gabriel/entities/Item.java
@@ -1,11 +1,15 @@
 package com.gabriel.entities;
 
+import javax.faces.annotation.ManagedProperty;
+import java.util.Objects;
+
 /**
  * @author daohn
  * @since 21/06/2021
  */
 public class Item {
 
+  @ManagedProperty(value = "#{item.produto}")
   private Produto produto;
   private Integer quantidade;
 
@@ -15,6 +19,10 @@ public class Item {
   }
 
   public Item() {
+  }
+
+  public static Item createItemWithZeroQuantidade(Produto produto) {
+    return new Item(produto, 0);
   }
 
   public Produto getProduto() {
@@ -33,5 +41,23 @@ public class Item {
     this.quantidade = quantidade;
   }
 
+  public void incrementQuantidade() {
+    this.quantidade += 1;
+  }
+
+  public Double valorTotal() {
+    return this.quantidade * this.produto.getPreco().doubleValue();
+  }
+
+  @Override public boolean equals(Object o) {
+    if(this == o) return true;
+    if(o == null || getClass() != o.getClass()) return false;
+    Item item = (Item) o;
+    return produto.equals(item.produto) && quantidade.equals(item.quantidade);
+  }
+
+  @Override public int hashCode() {
+    return Objects.hash(produto, quantidade);
+  }
 }
 
